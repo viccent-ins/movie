9b25ad7747f71b6a165735a12c0bffc078a23176
San John-lyly <sanjohn.in@gmail.com>
2023-08-10 19:51:01 +0700

    forward rename

--- app/Http/Controllers/API/DepositManagementController.php
+++ app/Http/Controllers/API/DepositManagementController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\BaseResponseController;
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\FileHelperController;
 use App\Models\DepositManagement;
 use Exception;
--- database/migrations/2023_08_10_084537_create_deposit_management_table.php
+++ database/migrations/2023_08_10_084537_create_deposit_management_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('deposit_managements', function (Blueprint $table) {
+        Schema::create('deposit_management', function (Blueprint $table) {
             $table->id();
             $table->integer('deposit_amount');
             $table->string('deposit_name');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('deposit_managements');
+        Schema::dropIfExists('deposit_management');
     }
 };
