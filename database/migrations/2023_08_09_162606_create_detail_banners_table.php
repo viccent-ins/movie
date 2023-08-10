d3fbfc802124c95505686ac0ab4cc8652766154f
San John-lyly <sanjohn.in@gmail.com>
2023-08-10 10:52:54 +0700

    implement migration

---
+++ database/migrations/2023_08_09_162606_create_detail_banners_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detail_banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('ban_title');
+            $table->smallInteger('ban_star');
+            $table->string('ban_desc');
+            $table->smallInteger('ban_minute');
+            $table->string('ban_file');
+            $table->string('ban_bg_file');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detail_banners');
+    }
+};
